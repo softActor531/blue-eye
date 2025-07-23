3bda3206bde84f87d4c15eef32045ccb0d22d6fd
SoftActor531 <champion00531@yahoo.com>
2025-07-23 15:49:32 -0500

    get only ethernet ip

--- app.js
+++ app.js
@@ -46,9 +46,11 @@ function getMacAddress() {
 function getLocalIP() {
   const interfaces = os.networkInterfaces();
   for (const name of Object.keys(interfaces)) {
+    if (!name.toLowerCase().includes('ethernet')) continue;
+
     for (const iface of interfaces[name]) {
       if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address; // Return the first non-internal IPv4
+        return iface.address;
       }
     }
   }
